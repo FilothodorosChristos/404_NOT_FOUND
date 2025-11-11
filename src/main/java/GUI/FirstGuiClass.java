bd060de098fedc1bd26e3af3d1bdb98c0f55b45d
Georgealex5555 <t8240005@aueb.gr>
2025-11-11 21:41:14 +0200

    add second window

--- src/main/java/GUI/FirstGuiClass.java
+++ src/main/java/GUI/FirstGuiClass.java
@@ -45,13 +45,75 @@ public class FirstGuiClass {
                 }
             };
             frame.add(imagePanel, BorderLayout.CENTER);
-            // NEXT BUTTON 1
+
             JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
             JButton button = new JButton("Επόμενο >");
             buttonPanel.add(button);
             frame.add(buttonPanel, BorderLayout.SOUTH);
 
+            button.addActionListener(e -> {
+                new SecondWindow();
+                frame.dispose();
+            });
+
             frame.setVisible(true);
         });
     }
 }
+
+// second window frame with "proypologismous" of 3 individual years
+class SecondWindow extends JFrame {
+    public SecondWindow() {
+        setTitle("GoverLens");
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+
+        JLabel lbl1 = new JLabel("Επίλεξε έτος");
+        lbl1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lbl1.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JLabel lbl2 = new JLabel("προϋπολογισμού");
+        lbl2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+        lbl2.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton btn2023 = new JButton("2023");
+        btn2023.setForeground(new Color(0, 0, 0));
+        btn2023.setBackground(new Color(70, 130, 180));
+        btn2023.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        btn2023.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton btn2024 = new JButton("2024");
+        btn2024.setForeground(new Color(0, 0, 0));
+        btn2024.setBackground(new Color(70, 130, 180));
+        btn2024.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        btn2024.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton btn2025 = new JButton("2025");
+        btn2025.setForeground(new Color(0, 0, 0));
+        btn2025.setBackground(new Color(70, 130, 180));
+        btn2025.setFont(new Font("Tahoma", Font.PLAIN, 15));
+        btn2025.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+       
+        panel.add(Box.createVerticalStrut(20));
+        panel.add(lbl1);
+        panel.add(lbl2);
+        panel.add(Box.createVerticalStrut(20));
+        panel.add(btn2023);
+        panel.add(Box.createVerticalStrut(10));
+        panel.add(btn2024);
+        panel.add(Box.createVerticalStrut(10));
+        panel.add(btn2025);
+
+        add(panel);
+        setVisible(true);
+    }
+}
+
+
+
