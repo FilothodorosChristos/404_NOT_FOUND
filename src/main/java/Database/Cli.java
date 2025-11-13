0a0356e63ace83a79e68c2092d2f8a389b9eda2f
MariaEliza <t8240110@aueb.gr>
2025-11-13 18:50:23 +0200

    Rename folder database

--- src/main/java/Database/Cli.java
+++ src/main/java/Database/Cli.java
@@ -1,9 +1,12 @@
 package database;
+import java.util.Scanner;
 
 public class Cli {
-
-    public static void main(String[] args) {
-          
+    private int answer;
+    public void menu() {
+        Scanner myScan = new Scanner(System.in);
+        System.out.printf("Καλωσορίσατε στο Goverlens!\n Θέλετε να ξεκινήσετε νέα προσωομίωση; \n 1.Ναι \n 2.Όχι \n");
+        answer = myScan.nextInt();
         DatabaseSetup.setDatabase();
         DataImporter.importer();
     }
