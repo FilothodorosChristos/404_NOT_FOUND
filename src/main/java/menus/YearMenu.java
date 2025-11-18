e7f56671cd2f8ebbcc5e444d6c7600e2a4dc84b5
MariaEliza <t8240110@aueb.gr>
2025-11-18 22:32:54 +0200

    Create class YearMenu that extends Menu

---
+++ src/main/java/menus/YearMenu.java
@@ -0,0 +1,27 @@
+public class YearMenu extends Menu {
+    
+    public YearMenu(UserIO io) {
+        super(io);
+    }
+
+    public void show() {
+        showMenu("""
+            --- Επιλέξτε έτος ---
+            1) 2023
+            2) 2024
+            3) 2025
+            4) Πίσω
+        """, 4);
+    }
+
+   @Override
+protected void handleChoice(int option) {
+    switch (option) {
+        case 1 -> new ActionsMenu(IO, 2023).show();
+        case 2 -> new ActionsMenu(IO, 2024).show();
+        case 3 -> new ActionsMenu(IO, 2025).show();
+        case 4 -> new MainMenu(IO).show();
+        default -> IO.showMessage("Μη έγκυρη επιλογή.");
+    }
+}
+}
