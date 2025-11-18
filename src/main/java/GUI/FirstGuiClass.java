bdd059c96937f75c23b9c315ed02660953654962
Katerina <t8240085@aueb.gr>
2025-11-18 17:58:24 +0200

    Update FirstGuiClass.java

--- src/main/java/GUI/FirstGuiClass.java
+++ src/main/java/GUI/FirstGuiClass.java
@@ -24,7 +24,7 @@ class ImagePanel extends JPanel {
     }
 }
 
-public class FirstGuiClass {
+public class FirstGuiClas {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             // MAIN FRAME
@@ -52,7 +52,7 @@ public class FirstGuiClass {
                     Graphics2D g2 = (Graphics2D) g.create();
                     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-                    int diameter = (int)(Math.min(getWidth(), getHeight()) * 0.4);
+                    int diameter = (int) (Math.min(getWidth(), getHeight()) * 0.4);
 
                     int x = (getWidth() - diameter) / 2;
                     int y = (getHeight() - diameter) / 3;
@@ -69,89 +69,81 @@ public class FirstGuiClass {
                 }
             };
 
-          
-          JPanel textPanel = new JPanel(); 
-          textPanel.setOpaque(false);
-           textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
+            JPanel textPanel = new JPanel();
+            textPanel.setOpaque(false);
+            textPanel.setLayout(new BoxLayout(textPanel, BoxLayout.Y_AXIS));
 
             JLabel paragraph = new JLabel("Καλώς ήρθατε στην GoverLens! ");
-           paragraph.setFont(new Font("Tahoma", Font.PLAIN, 50));
-           paragraph.setForeground(Color.WHITE);
-           paragraph.setAlignmentX(Component.CENTER_ALIGNMENT);
+            paragraph.setFont(new Font("Tahoma", Font.PLAIN, 50));
+            paragraph.setForeground(Color.WHITE);
+            paragraph.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-           textPanel.add(Box.createVerticalStrut(40)); // λίγο κενό πάνω
-           textPanel.add(paragraph);
-           panel1.add(textPanel, BorderLayout.CENTER);
-          
-           JPanel bottomTextPanel = new JPanel();
-           bottomTextPanel.setOpaque(false);
-           bottomTextPanel.setLayout(new BoxLayout(bottomTextPanel, BoxLayout.Y_AXIS));
+            textPanel.add(Box.createVerticalStrut(40)); // λίγο κενό πάνω
+            textPanel.add(paragraph);
+            panel1.add(textPanel, BorderLayout.CENTER);
 
-           JLabel paragraph2 = new JLabel("________________________________________________________");
+            JPanel bottomTextPanel = new JPanel();
+            bottomTextPanel.setOpaque(false);
+            bottomTextPanel.setLayout(new BoxLayout(bottomTextPanel, BoxLayout.Y_AXIS));
 
-           paragraph2.setFont(new Font("Tahoma", Font.PLAIN, 24));
-           paragraph2.setForeground(Color.WHITE);
-           paragraph2.setAlignmentX(Component.CENTER_ALIGNMENT);
+            JLabel paragraph2 = new JLabel("________________________________________________________");
 
-           bottomTextPanel.add(Box.createVerticalStrut(80)); 
-           
-           bottomTextPanel.add(paragraph2);
+            paragraph2.setFont(new Font("Tahoma", Font.PLAIN, 24));
+            paragraph2.setForeground(Color.WHITE);
+            paragraph2.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-           JLabel paragraph3 = new JLabel("Περισσότερη διαφάνεια , λιγότερη δυσκολία!");
-           paragraph3.setFont(new Font("Tahoma", Font.PLAIN, 24));
-           paragraph3.setForeground(Color.WHITE);
-           paragraph3.setAlignmentX(Component.CENTER_ALIGNMENT);
+            bottomTextPanel.add(Box.createVerticalStrut(80));
 
-           JLabel paragraph4 = new JLabel("Μάθε που πηγαίνει κάθε ευρώ και επεξεργάσου τα δεδομένα σαν υπουργός .");
-           paragraph4.setFont(new Font("Tahoma", Font.PLAIN, 24));
-           paragraph4.setForeground(Color.WHITE);
-           paragraph4.setAlignmentX(Component.CENTER_ALIGNMENT);
-           
+            bottomTextPanel.add(paragraph2);
 
-           bottomTextPanel.add(Box.createVerticalStrut(40));  
-           bottomTextPanel.add(paragraph3);
-           bottomTextPanel.add(Box.createVerticalStrut(10));
-           bottomTextPanel.add(paragraph4);
-           bottomTextPanel.add(Box.createVerticalStrut(80));
+            JLabel paragraph3 = new JLabel("Περισσότερη διαφάνεια , λιγότερη δυσκολία!");
+            paragraph3.setFont(new Font("Tahoma", Font.PLAIN, 24));
+            paragraph3.setForeground(Color.WHITE);
+            paragraph3.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-           JPanel buttonPanel1 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
-           buttonPanel1.setOpaque(false);
-           JButton next1 = new JButton("Επόμενο >");
-           buttonPanel1.add(next1);
+            JLabel paragraph4 = new JLabel("Μάθε που πηγαίνει κάθε ευρώ και επεξεργάσου τα δεδομένα σαν υπουργός .");
+            paragraph4.setFont(new Font("Tahoma", Font.PLAIN, 24));
+            paragraph4.setForeground(Color.WHITE);
+            paragraph4.setAlignmentX(Component.CENTER_ALIGNMENT);
 
+            bottomTextPanel.add(Box.createVerticalStrut(40));
+            bottomTextPanel.add(paragraph3);
+            bottomTextPanel.add(Box.createVerticalStrut(10));
+            bottomTextPanel.add(paragraph4);
+            bottomTextPanel.add(Box.createVerticalStrut(80));
 
-           JPanel southWrapper = new JPanel();
-           southWrapper.setLayout(new BorderLayout());
-           southWrapper.setOpaque(false);
-          
+            JPanel buttonPanel1 = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+            buttonPanel1.setOpaque(false);
+            JButton next1 = new JButton("Επόμενο >");
+            buttonPanel1.add(next1);
 
-           southWrapper.add(bottomTextPanel, BorderLayout.CENTER);
-           southWrapper.add(buttonPanel1, BorderLayout.SOUTH);
+            JPanel southWrapper = new JPanel();
+            southWrapper.setLayout(new BorderLayout());
+            southWrapper.setOpaque(false);
 
-           panel1.add(southWrapper, BorderLayout.SOUTH);
- 
+            southWrapper.add(bottomTextPanel, BorderLayout.CENTER);
+            southWrapper.add(buttonPanel1, BorderLayout.SOUTH);
 
+            panel1.add(southWrapper, BorderLayout.SOUTH);
 
             // PANEL 2 - YEAR SELECTION
 
-            
-
             class RoundedBorder extends AbstractBorder {
-            private int radius;
+                private int radius;
 
-            public RoundedBorder(int radius) {
-            this.radius = radius;
-            }
+                public RoundedBorder(int radius) {
+                    this.radius = radius;
+                }
 
-           @Override
-            public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
-            Graphics2D g2 = (Graphics2D) g.create();
-            g2.setColor(Color.BLACK); // χρώμα περιγράμματος, μπορείς να αλλάξεις
-            g2.setStroke(new BasicStroke(2)); // πάχος περιγράμματος
-            g2.drawRoundRect(x, y, width-1, height-1, radius, radius);
-            g2.dispose();
+                @Override
+                public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+                    Graphics2D g2 = (Graphics2D) g.create();
+                    g2.setColor(Color.BLACK); // χρώμα περιγράμματος, μπορείς να αλλάξεις
+                    g2.setStroke(new BasicStroke(2)); // πάχος περιγράμματος
+                    g2.drawRoundRect(x, y, width - 1, height - 1, radius, radius);
+                    g2.dispose();
+                }
             }
-        }
             JPanel panel2 = new ImagePanel("BackroundPhoto.jpg");
 
             JPanel yearsPanel = new JPanel();
@@ -163,14 +155,13 @@ public class FirstGuiClass {
             lbl1.setFont(new Font("Tahoma", Font.PLAIN, 30));
             lbl1.setAlignmentX(Component.CENTER_ALIGNMENT);
 
-            
             JButton btn2023 = new JButton("2023");
             JButton btn2024 = new JButton("2024");
             JButton btn2025 = new JButton("2025");
 
-            JButton[] yearButtons = {btn2023, btn2024, btn2025};
+            JButton[] yearButtons = { btn2023, btn2024, btn2025 };
             for (JButton btn : yearButtons) {
-                btn.setForeground(new Color(0,0,128));
+                btn.setForeground(new Color(0, 0, 128));
                 btn.setBackground(new Color(255, 255, 255));
                 btn.setFont(new Font("Tahoma", Font.BOLD, 28));
                 btn.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -179,7 +170,7 @@ public class FirstGuiClass {
                 btn.setFocusPainted(true);
                 btn.addActionListener(e -> cardLayout.show(mainPanel, "panel3"));
             }
-            
+
             yearsPanel.add(Box.createVerticalStrut(20));
             yearsPanel.add(lbl1);
             yearsPanel.add(Box.createVerticalStrut(20));
@@ -202,57 +193,89 @@ public class FirstGuiClass {
             panel2.add(bottomPanel2, BorderLayout.SOUTH);
 
             // PANEL 3 - USER ACTIONS
+
             JPanel panel3 = new ImagePanel("BackroundPhoto.jpg");
-            
+            panel3.setLayout(new BorderLayout());
 
             JLabel label = new JLabel("Παρακαλώ επιλέξτε διαδικασία:");
             label.setForeground(Color.WHITE);
             label.setFont(new Font("Arial", Font.PLAIN, 40));
-            label.setVerticalAlignment(JLabel.CENTER);
-            label.setBorder(BorderFactory.createEmptyBorder(100, 20, 50, 50));
+            label.setAlignmentX(Component.CENTER_ALIGNMENT);
+            label.setBorder(BorderFactory.createEmptyBorder(30, 0, 40, 0));
+
+            Font btnFont = new Font("Arial", Font.BOLD, 40);
+            Dimension btnSize = new Dimension(300, 80);
 
             JButton viewButton = new JButton("Προβολή");
             JButton editButton = new JButton("Επεξεργασία");
-            viewButton.setBorder(new RoundedBorder(30)); // ακτίνα στρογγυλέματος
-            viewButton.setFocusPainted(false);
-            editButton.setBorder(new RoundedBorder(30)); // ακτίνα στρογγυλέματος
-            editButton.setFocusPainted(false);
-            viewButton.setForeground(new Color(0,0,128));  // αλλάζει το χρώμα του κειμένου σε άσπρο
-            editButton.setForeground(new Color(0,0,128));
+            JButton diagramButton = new JButton("Διαγράμματα");
+            JButton comparationButton = new JButton("Σύγκριση");
 
-            viewButton.setFont(new Font("Arial", Font.BOLD, 40));
-            editButton.setFont(new Font("Arial", Font.BOLD, 40));
-            viewButton.setMaximumSize(new Dimension(300, 70));
-            editButton.setMaximumSize(new Dimension(300, 70));
-            JPanel actionButtonsPanel = new JPanel();
-            actionButtonsPanel.setLayout(new BoxLayout(actionButtonsPanel, BoxLayout.X_AXIS)); // X_AXIS για οριζόντια διάταξη
-            actionButtonsPanel.setOpaque(false);
+            JButton[] allButtons = { viewButton, editButton, diagramButton, comparationButton };
 
-           // Προσθήκη glue για να πάνε τα κουμπιά στο κέντρo 
-           actionButtonsPanel.add(Box.createHorizontalGlue());
-           actionButtonsPanel.add(viewButton);
-           actionButtonsPanel.add(Box.createRigidArea(new Dimension(100, 0))); // διάστημα μεταξύ κουμπιών
-           actionButtonsPanel.add(editButton);
-           actionButtonsPanel.add(Box.createHorizontalGlue());
+            for (JButton b : allButtons) {
+                b.setBorder(new RoundedBorder(30));
+                b.setFocusPainted(false);
+                b.setForeground(new Color(0, 0, 128));
+                b.setFont(btnFont);
+                b.setMaximumSize(btnSize);
 
-            JPanel labelWithButtonsPanel = new JPanel();
-            labelWithButtonsPanel.setLayout(new BoxLayout(labelWithButtonsPanel, BoxLayout.Y_AXIS));
-            labelWithButtonsPanel.setOpaque(false);
-            labelWithButtonsPanel.add(label);
-            labelWithButtonsPanel.add(Box.createRigidArea(new Dimension(0, 20)));
-            labelWithButtonsPanel.add(actionButtonsPanel);
+                b.setPreferredSize(btnSize); // Ορίζει προτεινόμενο μέγεθος
+                b.setMinimumSize(btnSize); // Ορίζει ελάχιστο μέγεθος
+                b.setContentAreaFilled(true); // Να γεμίζει το background
+                b.setOpaque(true);
 
-            panel3.add(labelWithButtonsPanel, BorderLayout.WEST);
+            }
+            JPanel buttonsGridPanel = new JPanel(new GridBagLayout());
+            buttonsGridPanel.setOpaque(false);
+
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.insets = new Insets(20, 30, 20, 30);
+
+            
+            gbc.gridx = 0;
+            gbc.gridy = 0;
+            buttonsGridPanel.add(viewButton, gbc);
+
+            gbc.gridx = 1;
+            gbc.gridy = 0;
+            buttonsGridPanel.add(editButton, gbc);
+
+            gbc.gridx = 0;
+            gbc.gridy = 1;
+            buttonsGridPanel.add(diagramButton, gbc);
+
+            gbc.gridx = 1;
+            gbc.gridy = 1;
+            buttonsGridPanel.add(comparationButton, gbc);
+
+            // Panel για να κεντράρει το label και τα κουμπιά ΟΡΙΖΟΝΤΙΑ 
+            // αλλά παραμένουν ΠΑΝΩ
+            JPanel topCenterPanel = new JPanel();
+            topCenterPanel.setLayout(new BoxLayout(topCenterPanel, BoxLayout.Y_AXIS));
+            topCenterPanel.setOpaque(false);
+
+            label.setAlignmentX(Component.CENTER_ALIGNMENT);
+            buttonsGridPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+            topCenterPanel.add(Box.createRigidArea(new Dimension(0, 40))); 
+            topCenterPanel.add(label);
+            topCenterPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+            topCenterPanel.add(buttonsGridPanel);
+
+            panel3.add(topCenterPanel, BorderLayout.NORTH);
+
 
             JPanel bottomPanel3 = new JPanel(new BorderLayout());
             bottomPanel3.setOpaque(false);
             bottomPanel3.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
+
             JButton prev3 = new JButton("< Προηγούμενο");
             prev3.addActionListener(e -> cardLayout.show(mainPanel, "panel2"));
             bottomPanel3.add(prev3, BorderLayout.WEST);
+
             panel3.add(bottomPanel3, BorderLayout.SOUTH);
 
-            // ADD PANELS TO MAIN PANEL
             mainPanel.add(panel1, "panel1");
             mainPanel.add(panel2, "panel2");
             mainPanel.add(panel3, "panel3");
@@ -260,11 +283,10 @@ public class FirstGuiClass {
             frame.add(mainPanel);
             frame.setVisible(true);
 
-            // SHOW FIRST PANEL
             cardLayout.show(mainPanel, "panel1");
 
-            // BUTTON NEXT PANEL 1
             next1.addActionListener(e -> cardLayout.show(mainPanel, "panel2"));
+
         });
     }
 }
