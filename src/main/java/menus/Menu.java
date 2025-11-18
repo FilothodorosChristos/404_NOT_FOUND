acb528534691c907859c981538256fbcc10629d9
MariaEliza <t8240110@aueb.gr>
2025-11-18 22:31:32 +0200

    Create abstract class Menu

---
+++ src/main/java/menus/Menu.java
@@ -0,0 +1,24 @@
+public abstract class Menu {
+    protected final UserIo IO;
+
+    public Menu(UserIO io) {
+        this.IO = io;
+    }
+
+    // η μέθοδος που κάθε υπομενού πρέπει να υλοποιήσει
+    protected abstract void handleChoice(int choice);
+
+    // κοινή μέθοδος για εμφάνιση μενού και έλεγχο επιλογών
+    public void showMenu(String message, int optionsCount) {
+        int choice;
+        do {
+            IO.showMessage(message);
+            choice = IO.getInt("Επιλογή: ");
+            if (choice < 1 || choice > optionsCount) {
+                IO.showMessage("Μη έγκυρη επιλογή. Δώσε αριθμό 1-" + optionsCount);
+            }
+        } while (choice < 1 || choice > optionsCount);
+
+        handleChoice(choice);
+    }
+}
