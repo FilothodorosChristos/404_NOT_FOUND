3268eb841be59eb6174a3cd631bfd3e5b0bac476
MariaEliza <t8240110@aueb.gr>
2025-12-03 22:40:41 +0200

    Add Log and LogDao class

---
+++ src/main/java/dao/LogDao.java
@@ -0,0 +1,44 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import database.DatabaseSetup;
+
+public class LogDao {
+
+      private Log mapRow(ResultSet rs) throws SQLException {
+    return new Log(
+                rs.getInt("id"),
+                rs.getString("table_name"),
+                rs.getString("operation"),
+                rs.getInt("row_id"),
+                rs.getString("old_data"),
+                rs.getString("new_data"),
+                rs.getString("timestamp")
+    );
+  }
+    
+    public List<Log> selectLog(int year, String type) {
+    List<Log> log = new ArrayList<>();
+    final String SQL = "SELECT * FROM log";
+    try (Connection connection = DatabaseSetup.getConnection();
+             PreparedStatement statement = connection.prepareStatement(SQL)) {
+
+      try (ResultSet resultSet = statement.executeQuery()) {
+        while (resultSet.next()) {
+          log.add(mapRow(resultSet));
+        }
+      }
+
+    } catch (SQLException e) {
+      throw new RuntimeException("Σφάλμα στη βάση (selectCashFlow): " + e.getMessage(), e);
+    }
+
+    return log;
+  }
+}
