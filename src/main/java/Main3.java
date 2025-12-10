c9beca0a8753a22a22cae5809fb472f59aba86ae
MariaEliza <t8240110@aueb.gr>
2025-12-10 20:56:57 +0200

    Modify Main3

--- src/main/java/Main3.java
+++ src/main/java/Main3.java
@@ -1,9 +1,7 @@
 import service.SimulationService;
-//import database.DataImporter;
 
 public class Main3 {
     public static void main(String args[]) {
-    //DataImporter.importer();
         //SimulationService.startIfDatabaseMissing();
         SimulationService.startNewSimulation();
     }
