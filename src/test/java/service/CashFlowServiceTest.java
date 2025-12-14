629a1f2257a7efa096a41a9b0f710ba4b307e80c
MariaEliza <t8240110@aueb.gr>
2025-12-15 00:52:27 +0200

    Modify Service tests so that all test files are deleted after completion

--- src/test/java/service/CashFlowServiceTest.java
+++ src/test/java/service/CashFlowServiceTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.*;
 import dao.CashFlow;
 import dao.CashFlowDao;
 import database.DatabaseSetup;
+
+import java.io.File;
 import java.util.List;
 import org.junit.jupiter.api.*;
 
@@ -12,6 +14,7 @@ public class CashFlowServiceTest {
 
   private static final String ORIGINAL_URL = "jdbc:sqlite:budgetDB.db";
   private static final String TEST_URL = "jdbc:sqlite:test_cashflow.db";
+  private static final String TEST_FILE = "test_cashflow.db";
 
   private CashFlowService service;
 
@@ -29,9 +32,13 @@ public class CashFlowServiceTest {
     service = new CashFlowService();
   }
 
-  @AfterEach
-  void tearDown() {
+  @AfterAll
+  static void tearDown() {
     DatabaseSetup.setURL(ORIGINAL_URL);
+    File f = new File(TEST_FILE); 
+    if (f.exists()) {
+      f.delete();
+    }
   }
 
   // -------------------------------------------------------------
