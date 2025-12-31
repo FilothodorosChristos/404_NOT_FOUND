b44349a450e342c54daca4f523fd66ecd71c515b
MariaEliza <t8240110@aueb.gr>
2025-12-31 18:50:13 +0200

    Increase jacoco test coverage for DbExistsChecker

--- src/main/java/dao/ForeisDao.java
+++ src/main/java/dao/ForeisDao.java
@@ -2,7 +2,6 @@ package dao;
 
 import database.DatabaseSetup;
 import dto.ForeasCompareDto;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
--- src/main/java/service/CashFlowService.java
+++ src/main/java/service/CashFlowService.java
@@ -33,6 +33,13 @@ public class CashFlowService {
     ValidationUtils.validateYear(cashflow.getYearId());
 
     cashflowDao.addCashFlow(cashflow);
+    List<CashFlow> existing = cashflowDao.selectCashFlow(
+        cashflow.getYearId(), "Έσοδο"
+    );
+    existing.addAll(cashflowDao.selectCashFlow(
+        cashflow.getYearId(), "Έξοδο"
+    ));
+    ValidationUtils.printBudgetStatus(existing);
   }
 
   /**
